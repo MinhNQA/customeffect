9e944a9052475ce489bfb9db0c8c23f09c40f3a9
MinhNQA <anhminh7m2@gmail.com>
2025-07-07 20:52:45 +0700

    hi

---
+++ src/main/java/com/customeffect/VoidDomain.java
@@ -0,0 +1,30 @@
+package com.customeffect;
+
+import net.advancedplugins.ae.api.custom.AdvancedEffect;
+import net.advancedplugins.ae.api.custom.ExecutionTask;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+
+public class VoidDomain extends AdvancedEffect {
+
+    public VoidDomain(JavaPlugin plugin) {
+        super(plugin, "void_domain", "Void Domain", "Damages and shows breath", 1);
+    }
+
+    @Override
+    public boolean executeEffect(ExecutionTask task, LivingEntity user, String[] args) {
+        if (!(user instanceof Player)) return false;
+
+        Location loc = user.getLocation();
+        World world = loc.getWorld();
+
+        for (Entity e : world.getNearbyEntities(loc, 2.5, 2.5, 2.5)) {
+            if (e instanceof LivingEntity && !e.equals(user)) {
+                ((LivingEntity) e).damage(4.0, user);
+                world.spawnParticle(Particle.DRAGON_BREATH, e.getLocation().add(0, 1, 0), 30, 0.3, 0.3, 0.3, 0);
+            }
+        }
+
+        return true;
+    }
+}
